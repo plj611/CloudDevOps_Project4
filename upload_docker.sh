34c387029ec751e143f00e3fd30873653d6ad78f
Plaw <plj611@outlook.com>
2021-08-26 15:50:24 +0800

    add upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="plj611/project4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag project4:latest $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest

99666c8ffed7bf33420e0ee22659ffe597847348
Plaw <plj611@outlook.com>
2021-08-26 18:32:15 +0800

    add run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,13 +5,15 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="plj611/project4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+./kubectl run project4 --image=$dockerpath:latest --port=80
 
 # Step 3:
 # List kubernetes pods
+./kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
